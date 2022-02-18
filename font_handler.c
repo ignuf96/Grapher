06413500cc4024a3a74481d69b8bfc9fdf5b9dfa
Jonathan Rivas <ignuf96@gmail.com>
2022-02-18 07:54:37 -0700

    added numbers and fps counter

--- font_handler.c
+++ font_handler.c
@@ -1,3 +1,14 @@
+/* PURPOSE:
+** Library with helper functions that convert any string into a texture.
+**
+** TODO:
+** 1. Make library convert to any font-type and any size and any color.
+** 2. Container for the storage of multiple fonts.
+** 3. Cleanup function to aleviate the burden of cleaning up dynamic storage from caller. Will cleanup container and cycle through font container.
+**
+**
+ */
+
 #include "font_handler.h"
 
 #include <SDL2/SDL.h>
@@ -6,142 +17,30 @@
 #include <stdbool.h>
 #include <limits.h>
 
-#define MAX_FONTS  62
-#define FONT_SIZE 24
-#define FONT_SIZES 64
+#define MAX_FONTS 10
 
-// Constants used to access array of the alphabet and numbers 0-9
-// Ugly and verbose. Fix later
-const int
-    LETTER_A = 0,
-    LETTER_B = 1,
-    LETTER_C = 2,
-    LETTER_D = 3,
-    LETTER_E = 4,
-    LETTER_F = 5,
-    LETTER_G = 6,
-    LETTER_H = 7,
-    LETTER_I = 8,
-    LETTER_J = 9,
-    LETTER_K = 10,
-    LETTER_L = 11,
-    LETTER_M = 12,
-    LETTER_N = 13,
-    LETTER_O = 14,
-    LETTER_P = 15,
-    LETTER_Q = 16,
-    LETTER_R = 17,
-    LETTER_S = 18,
-    LETTER_T = 19,
-    LETTER_U = 20,
-    LETTER_V = 21,
-    LETTER_W = 22,
-    LETTER_X = 23,
-    LETTER_Y = 24,
-    LETTER_Z = 25,
-
-    UPPER_LETTER_A = 26,
-    UPPER_LETTER_B = 27,
-    UPPER_LETTER_C = 28,
-    UPPER_LETTER_D = 29,
-    UPPER_LETTER_E = 30,
-    UPPER_LETTER_F = 31,
-    UPPER_LETTER_G = 32,
-    UPPER_LETTER_H = 33,
-    UPPER_LETTER_I = 34,
-    UPPER_LETTER_J = 35,
-    UPPER_LETTER_K = 36,
-    UPPER_LETTER_L = 37,
-    UPPER_LETTER_M = 38,
-    UPPER_LETTER_N = 39,
-    UPPER_LETTER_O = 40,
-    UPPER_LETTER_P = 41,
-    UPPER_LETTER_Q = 42,
-    UPPER_LETTER_R = 43,
-    UPPER_LETTER_S = 44,
-    UPPER_LETTER_T = 45,
-    UPPER_LETTER_U = 46,
-    UPPER_LETTER_V = 47,
-    UPPER_LETTER_W = 48,
-    UPPER_LETTER_X = 49,
-    UPPER_LETTER_Y = 50,
-    UPPER_LETTER_Z = 51,
-
-    NUMBER_0 = 52,
-    NUMBER_1 = 53,
-    NUMBER_2 = 54,
-    NUMBER_3 = 55,
-    NUMBER_4 = 56,
-    NUMBER_5 = 57,
-    NUMBER_6 = 58,
-    NUMBER_7 = 59,
-    NUMBER_8 = 60,
-    NUMBER_9 = 61;
-
-const char alphanum[MAX_FONTS][2] = {
-{"a"}, {"b"}, {"c"}, {"d"}, {"e"}, {"f"}, {"g"}, {"h"}, {"i"}, {"j"}, {"k"}, {"l"}, {"m"},
-{"n"}, {"o"}, {"p"}, {"q"}, {"r"}, {"s"}, {"t"}, {"u"}, {"v"}, {"w"}, {"x"}, {"y"}, {"z"},
-
-{"A"}, {"B"}, {"C"}, {"D"}, {"E"}, {"F"}, {"G"}, {"H"}, {"I"}, {"J"}, {"K"}, {"L"}, {"M"},
-{"N"}, {"O"}, {"P"}, {"Q"}, {"R"}, {"S"}, {"T"}, {"U"}, {"V"}, {"W"}, {"X"}, {"Y"}, {"Z"},
-{"0"}, {"1"}, {"2"}, {"3"}, {"4"}, {"5"}, {"6"}, {"7"}, {"8"}, {"9"},
-};
-
-static SDL_Texture *font_textures[FONT_SIZES][MAX_FONTS];
 static bool fonts_loaded = false;
-
-SDL_Rect rect[FONT_SIZES][MAX_FONTS];
+static TTF_Font *coordinate_font;
 
 void font_init(void)
 {
-   TTF_Init();
-}
-
-void load_fonts(SDL_Renderer *renderer, const char *path)
-{
-    if(!fonts_loaded)
-    {
-        //TTF_Font *font = TTF_OpenFont(path, FONT_SIZE);
-        for(int i = 0; i < FONT_SIZES; i++)
-        {
-            TTF_Font *font = TTF_OpenFont(path, i);
-            for(int j=0; j < MAX_FONTS; j++)
-            {
-            SDL_Surface *surface;
-            SDL_Color textColor = { 0, 255, 0, 255};
-
-            surface = TTF_RenderText_Solid(font, alphanum[i], textColor);
-            font_textures[i][j] = SDL_CreateTextureFromSurface(renderer, surface);
-
-            rect[i][j].x = 0;
-            rect[i][j].y = 0;
-            rect[i][j].w = surface->w;
-            rect[i][j].h = surface->h;
-
-            SDL_FreeSurface(surface);
-
-            //TTF_CloseFont(font);
-            }
-        }
-        fonts_loaded = true;
-    }
+    TTF_Init();
+    fonts_loaded = true;
+    char* path = "OpenSans-Regular.ttf";
+    coordinate_font = TTF_OpenFont(path, 24);
 
 }
 
-//struct temp_font myfont;
+struct temp_font myfont;
 
-
-struct temp_font load_string_font(SDL_Renderer* renderer, const char *path, char* str)
+struct temp_font load_string_font(SDL_Renderer* renderer, char* str)
 {
-    struct temp_font myfont;
-
     if(fonts_loaded)
     {
-        TTF_Font *font = TTF_OpenFont(path, 24);
         SDL_Surface *surface;
         SDL_Color textColor = { 0, 255, 0, 255};
 
-        surface = TTF_RenderText_Solid(font, str, textColor);
+        surface = TTF_RenderText_Solid(coordinate_font, str, textColor);
         myfont.texture = SDL_CreateTextureFromSurface(renderer, surface);
 
         myfont.rect.x = 0;
@@ -149,86 +48,17 @@ struct temp_font load_string_font(SDL_Renderer* renderer, const char *path, char
         myfont.rect.w = surface->w;
         myfont.rect.h = surface->h;
 
+        //SDL_DestroyTexture(myfont.texture);
         SDL_FreeSurface(surface);
-        TTF_CloseFont(font);
 
     }
     return myfont;
-}
-
-SDL_Texture* get_font_texture(int font_number, int font_size)
-{
-    // Does this cause a memory leak??
-    SDL_Texture *temp_texture = NULL;
-
-    if(fonts_loaded)
-        temp_texture = font_textures[font_size][font_number];
-
-    return temp_texture;
 }
 
 void font_cleanup(void)
 {
     if(fonts_loaded)
     {
-        for(int i = 0; i < FONT_SIZES; i++)
-        {
-            for(int j = 0; j < MAX_FONTS; j++)
-            {
-            SDL_DestroyTexture(font_textures[i][j]);
-            }
-        }
+        TTF_CloseFont(coordinate_font);
     }
 }
-
-
-//int allnumbers[FONT_SIZES][INT_MAX];
-
-void make_all_numbers_to_fonts(void)
-{
-
-}
-
-/* definitely change
-int new_font_draw(SDL_Renderer* renderer, SDL_Texture texture, int x, int y)
-{
-   //SDL_RenderCopy(renderer, texture);
-}
-*/
-
-int font_draw(SDL_Renderer *renderer, int font_num, int font_size, int x, int y)
-{
-    int error = 0;
-
-    if(font_size > FONT_SIZES | font_size < 0)
-    {
-        error = 1;
-    } else {
-
-    rect[font_size][font_num].x = x;
-    rect[font_size][font_num].y = y;
-    SDL_RenderCopy(renderer, get_font_texture(font_num, font_size), NULL, &rect[font_size][font_num]);
-    }
-
-    return error;
-}
-/* Do this later, too lazy right now, making a shit ton of fonts for now, removing later(hopefully) */
-/*
-SDL_Texture* conv_num_to_font(int number, int font_size)
-{
-    if(number < 10 && number >= 0)
-    {
-
-    }
-    int divisor = 10;
-    int result = number;
-
-    do
-    {
-
-    }
-
-
-    for(int divisor = 10; (divisor )
-}
-*/
--- main.c
+++ main.c
@@ -1,9 +1,17 @@
-// hello comment
+/* TODO
+**
+**  1. Pixel independent world units. This is needed to add numbers to graph, scrolling, coordinates and will increase portability.
+**  2. Implement a constant frame-rate loop. Possibly 30-60 fps. This will help with smoothness of mouse gestures.
+**  3. Fix mouse gesture when swiping(currently inconsistent)
+**  4. Implement actual coordinates for entire graph.
+*/
+
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <SDL2/SDL_image.h>
+#include <unistd.h> // For sleep();
 
 // Font things
 #include "font_handler.h"
@@ -15,12 +23,8 @@
 #define WINDOW_POSX SDL_WINDOWPOS_UNDEFINED
 #define WINDOW_POSY SDL_WINDOWPOS_UNDEFINED
 
-#define ORIGIN_OFFSET_X 0
-#define ORIGIN_OFFSET_Y 1
-
 SDL_Window* window;
 SDL_Renderer* renderer;
-SDL_Renderer* font_renderer;
 SDL_Event e;
 
 int window_width = STARTING_WINDOW_WIDTH;
@@ -31,49 +35,82 @@ struct INT_VECTOR2 {
 	int y;
 };
 
+struct FLOAT_VECTOR2 {
+	float x;
+	float y;
+};
+
 struct INT_VECTOR2 origin = {STARTING_WINDOW_WIDTH/2, STARTING_WINDOW_HEIGHT/2};
 struct INT_VECTOR2 origin_offset;
-struct INT_VECTOR2 mouse_speed = {200, 120};
+struct FLOAT_VECTOR2 mouse_speed = {12, 6};
 struct INT_VECTOR2 coordinate_location;
 
 // Variables for keeping track of how far away we're from the origin
 // Used for drawing graph
 // Amount of space away from y and x axis
 // Used for drawing lines in the quadrants
-static float spacing = 1.0f;
+static float spacing = 40.0f;
 
-static int render_distance = 1000;
+struct INT_VECTOR2 render_distance = {STARTING_WINDOW_WIDTH, STARTING_WINDOW_HEIGHT};
 
 bool quit = false;
 
-void initialize(void);
-void input(void);
-void cleanup(void);
-void mouse_event(void);
-void draw(void);
-void draw_axes(void);
-void draw_coordinates(void);
+static void initialize(void);
+static void input(void);
+static void cleanup(void);
+static void mouse_event(void);
+static void draw(void);
+static void draw_axes(void);
+static void draw_coordinates(void);
+static void draw_numbers(void);
 
 char* coordinate_str_x = "Coordinate X: ";
 char* coordinate_str_y = "Coordinate Y: ";
 
 // find out limit of an array size(does type matter for array size??)
-#define MAX_COORD_SIZE 20000
+#define MAX_COORD_SIZE 250
 char coordinate_x[MAX_COORD_SIZE];
 char coordinate_y[MAX_COORD_SIZE];
 
+float fps;
+const float FPS_TARGET = 60.0f;
+
 int main(void)
 {
-
 	initialize();
+	//ADAPTIVE SYNC (-1) IMMEDIATE(0)
 	SDL_GL_SetSwapInterval(1);
-
-	load_fonts(renderer, "OpenSans-Regular.ttf");
 
 	while(!quit)
 	{
+		// Ripped of internet fps counter
+		Uint32 start_time = SDL_GetTicks();
+		Uint32 frame_time;
+		//float fps;
+
 		input();
 		draw();
+
+		frame_time = SDL_GetTicks() - start_time;
+
+
+		fps = (frame_time > 0) ? 1000.0f / frame_time : 0.0f;
+		// Draw fps
+		char buffconv[MAX_COORD_SIZE] = {0};
+		strncat(buffconv, "FPS: ", MAX_COORD_SIZE-1);
+		snprintf(&buffconv[5], MAX_COORD_SIZE-5, "%d", (int)fps);
+		struct temp_font temp = load_string_font(renderer, buffconv);
+		temp.rect.x = 200;
+		temp.rect.y = 300;
+
+		SDL_RenderCopy(renderer, temp.texture, NULL, &temp.rect);
+		SDL_RenderPresent(renderer);
+		SDL_DestroyTexture(temp.texture);
+		printf("Printing fps: %d\n", (int)fps);
+
+		if(fps > FPS_TARGET)
+			SDL_Delay((fps-FPS_TARGET)/1000);
+
 	}
 
 	cleanup();
@@ -83,17 +120,11 @@ int main(void)
 
 void draw(void)
 {
-	SDL_RenderSetScale(renderer, 2, 2);
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	SDL_RenderClear(renderer);
 
 	draw_axes();
-
-	font_draw(renderer, 22, NUMBER_0, (origin.x+origin_offset.x/mouse_speed.x) + 5, (origin.y+origin_offset.y/mouse_speed.y));
-
-	SDL_RenderSetScale(renderer, 1, 1);
 	draw_coordinates();
-
 	SDL_RenderPresent(renderer);
 }
 
@@ -106,38 +137,98 @@ bool left_mousedown = false;
 
 void draw_axes(void)
 {
-
 	coordinate_location.x = origin.x+origin_offset.x/mouse_speed.x;
 	coordinate_location.y = origin.y+origin_offset.y/mouse_speed.y;
 
 	SDL_SetRenderDrawColor(renderer, 145, 145, 145, 255);
 
-	for(int count = 0, y = coordinate_location.y + (-spacing); count < render_distance; y += -spacing, count++)
+	for(int count = 0, y = coordinate_location.y + (-spacing); count < render_distance.y; y += -spacing, count++)
 	{
 		SDL_RenderDrawLine(renderer, 0, y, window_width, y);
 	}
 
-	for(int count = 0, y = coordinate_location.y + (spacing); count < render_distance; y += spacing, count++)
+	for(int count = 0, y = coordinate_location.y + (spacing); count < render_distance.y; y += spacing, count++)
 	{
 		SDL_RenderDrawLine(renderer, 0, y, window_width, y);
 	}
 
-	for(int count = 0, x = coordinate_location.x + (-spacing); count < render_distance; x += -spacing, count++)
+	for(int count = 0, x = coordinate_location.x + (-spacing); count < render_distance.x; x += -spacing, count++)
 	{
-		SDL_RenderDrawLine(renderer, x, 0, x, window_height);
+		SDL_RenderDrawLine(renderer, x, 0,  x, window_height);
 	}
 
-	for(int count = 0, x = coordinate_location.x + (spacing); count < render_distance; x += spacing, count++)
+	for(int count = 0, x = coordinate_location.x + (spacing); count < render_distance.x; x += spacing, count++)
 	{
 		SDL_RenderDrawLine(renderer, x, 0, x, window_height);
+
 	}
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
 	SDL_RenderDrawLine(renderer, 0, coordinate_location.y, window_width, coordinate_location.y);
 	SDL_RenderDrawLine(renderer, coordinate_location.x, 0, coordinate_location.x, window_height);
 
-
+	draw_numbers();
 }
+
+void draw_numbers(void)
+{
+	struct INT_VECTOR2 distance;
+	// positives up + 0
+	char buffconv[MAX_COORD_SIZE];
+	struct temp_font temp;
+	int count = 0;
+	for(distance.y = 0, distance.x = 0;
+		count < render_distance.y; distance.y += (-spacing), count++)
+	{
+		snprintf(buffconv, MAX_COORD_SIZE, "%d", count);
+		temp = load_string_font(renderer, buffconv);
+		temp.rect.x = distance.x + coordinate_location.x;
+		temp.rect.y = distance.y + coordinate_location.y;
+
+		SDL_RenderCopy(renderer, temp.texture, NULL, &temp.rect);
+		SDL_DestroyTexture(temp.texture);
+	}
+	count = 1;
+	// negatives down
+	for(distance.y = (spacing), distance.x = 0; count < render_distance.y; distance.y += spacing, count++)
+	{
+		snprintf(buffconv, MAX_COORD_SIZE, "%d", -count);
+		temp = load_string_font(renderer, buffconv);
+		temp.rect.x = distance.x + coordinate_location.x;
+		temp.rect.y = distance.y + coordinate_location.y;
+
+		SDL_RenderCopy(renderer, temp.texture, NULL, &temp.rect);
+		SDL_DestroyTexture(temp.texture);
+	}
+
+	count = 1;
+	// positive right
+	for(distance.y = 0, distance.x = (spacing); count < render_distance.x; distance.x += spacing, count++)
+	{
+		snprintf(buffconv, MAX_COORD_SIZE, "%d", count);
+		temp = load_string_font(renderer, buffconv);
+		temp.rect.x = distance.x + coordinate_location.x;
+		temp.rect.y = distance.y + coordinate_location.y;
+
+		SDL_RenderCopy(renderer, temp.texture, NULL, &temp.rect);
+		SDL_DestroyTexture(temp.texture);
+	}
+
+	count = -1;
+	// negatives left
+	// This is one is a bit different. Don't know why we have to specifically assign -1 to count and not on the negative down for loop
+	for(distance.y = 0, distance.x = (spacing); count < render_distance.x; distance.x += (-spacing), count++)
+	{
+		snprintf(buffconv, MAX_COORD_SIZE, "%d", -count);
+		temp = load_string_font(renderer, buffconv);
+		temp.rect.x = distance.x + coordinate_location.x;
+		temp.rect.y = distance.y + coordinate_location.y;
+
+		SDL_RenderCopy(renderer, temp.texture, NULL, &temp.rect);
+		SDL_DestroyTexture(temp.texture);
+	}
+}
+
 
 void mouse_event(void)
 {
@@ -146,26 +237,21 @@ void mouse_event(void)
 		int x, y;
 		SDL_GetMouseState(&x, &y);
 
-		printf("Left Mouse Down\n");
 		if(first_click == false)
 		{
-			printf("Left Mouse first click\n");
 			first_click = true;
 			first_click_x = x;
 			first_click_y = y;
 
-			printf("First Click x:%d\ny:%d\n", first_click_x, first_click_y);
-
 			first_click = true;
 		}
-		printf("X: %d\nY:%d\n", x, y);
 		coordinate_location.x = x;
 		coordinate_location.y = y;
 
 		// We get the difference between the coordinates from when
 		// the user first clicks the mouse and while they are moving the mouse
-		origin_offset.x += (first_click_x -x);
-		origin_offset.y += (first_click_y - y);
+		origin_offset.x += ((first_click_x -x) * (fps/1000));
+		origin_offset.y += ((first_click_y - y) * (fps/1000));
 	}
 
 }
@@ -188,13 +274,10 @@ void draw_coordinates(void)
 	snprintf(num_buffer, MAX_COORD_SIZE, "%d", coordinate_location.y);
 	char* final_str_y = strncat(coordinate_y, num_buffer, MAX_COORD_SIZE-1);
 
-	printf("Final String X: %s\n", final_str_x);
-	printf("Final String Y: %s\n", final_str_y);
-
-	struct temp_font temp_font_x = load_string_font(renderer, "OpenSans-Regular.ttf", final_str_x);
+	struct temp_font temp_font_x = load_string_font(renderer, final_str_x);
 	SDL_RenderCopy(renderer, temp_font_x.texture, NULL, &temp_font_x.rect);
 
-	struct temp_font temp_font_y = load_string_font(renderer, "OpenSans-Regular.ttf", final_str_y);
+	struct temp_font temp_font_y = load_string_font(renderer, final_str_y);
 	temp_font_y.rect.y = 75;
 	SDL_RenderCopy(renderer, temp_font_y.texture, NULL, &temp_font_y.rect);
 
@@ -207,15 +290,16 @@ void initialize(void)
 	SDL_Init(SDL_INIT_VIDEO);
 	window = SDL_CreateWindow(WINDOW_TITLE, WINDOW_POSX, WINDOW_POSY, STARTING_WINDOW_WIDTH, STARTING_WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
 
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-	SDL_RenderSetScale(renderer, 16, 9);
+	//SDL_RenderSetScale(renderer, 16, 9);
+	//SDL_RenderSetScale(renderer, 2, 2);
+	//SDL_RenderSetLogicalSize(renderer, 4*100, 3*100);
 
 	int buffering;
 	SDL_GL_GetAttribute(SDL_GL_DOUBLEBUFFER, &buffering);
 
 	font_init();
-
 }
 
 void input(void)
@@ -235,28 +319,24 @@ void input(void)
 				break;
 
 			case SDL_MOUSEBUTTONDOWN:
-				if(e.type & SDL_BUTTON_LMASK != 0)
+				if((e.type & SDL_BUTTON_LMASK) != 0)
 				{
 					left_mousedown = true;
 				}
-				printf("A Mouse button is down\n");
-				if(buttons & SDL_BUTTON_LMASK != 0)
+				if((buttons & SDL_BUTTON_LMASK) != 0)
 				{
 				}
 				break;
 
 			case SDL_MOUSEBUTTONUP:
-				printf("Button up!!!\n");
 				left_mousedown = false;
-					//if(e.type & SDL_BUTTON_LMASK != 0)
-					//{
-						printf("Left mouse disengaged\n");
-
+					if((e.type & SDL_BUTTON_LMASK) != 0)
+					{
 						first_click_x = 0;
 						first_click_y = 0;
 
 						first_click = false;
-					//}
+					}
 
 
 					break;
@@ -271,7 +351,6 @@ void input(void)
 					if(spacing > 0)
 					{
 						spacing--;
-						printf("Spacing: %d\n", spacing);
 					} else spacing = 0;
 				}
 
@@ -285,13 +364,14 @@ void input(void)
 						/*
 						** Change line coordinates to new window size
 						*/
-						printf("Window size changed to %dx%d\n", e.window.data1, e.window.data2);
-
 						window_width = e.window.data1;
 						window_height = e.window.data2;
 
 						origin.x = window_width/2;
 						origin.y = window_height/2;
+
+						render_distance.x = window_width/spacing;
+						render_distance.y = window_height/spacing;
 
 						break;
 				}
