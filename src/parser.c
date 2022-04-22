17100f2b725fdffc817afaa57a8e4ec0b2abe31f
johnny <ignuf96@gmail.com>
2022-04-22 14:09:58 -0700

    modified parser

--- src/parser.c
+++ src/parser.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "../include/datatypes.h"
 
+char *end_of_term = "+-/*";
 struct LINE_DATA parse_equation(char *equation, int length)
 {
+    int num_buffer[length];
+    int *nbuffer = &num_buffer[0];
+
     struct LINE_DATA line_data;
 
     char *c = equation;
@@ -20,12 +26,20 @@ struct LINE_DATA parse_equation(char *equation, int length)
         printf("Slope-intercept form\n");
     }
 
+    int skipped_first_term = 0;
     while((*c) == ' ' || (*c) == '=')
+    {
         c++;
+        skipped_first_term++;
+    }
 
     int count = 0;
 
     printf("Number is:  ");
+
+    char first_term_buff[length];
+    char *ft_buff = &first_term_buff[0];
+
     for(int i=0; i < max_tries; i++, c++)
     {
         switch (*c)
@@ -40,33 +54,69 @@ struct LINE_DATA parse_equation(char *equation, int length)
             case '7':
             case '8':
             case '9':
-            printf("%d", (*c)-'0');
-            count++;
-            break;
+    //            printf("%d", (*c)-'0');
+                *ft_buff = (*c);
+                ft_buff++;
+                count++;
+                break;
             case 'x':
-            i=max_tries;
-            break;
+                i=max_tries;
+                break;
         }
     }
     printf("\n");
 
-    c = &equation[0];
+    char *end;
+    int slopeconv = strtol(first_term_buff, NULL, 10);
 
-    for(int i=0; i < max_tries; i++, c++)
+    //c = &equation[count];
+    printf("Number contains %d digits\n", count);
+    printf("Number is %d\n", slopeconv);
+
+    printf("Skipping ");
+    while((*c) == ' ' || (*c) == '=' || (*c) == '+')
+    {
+        printf("%c", *c);
+        c++;
+    }
+    printf("\n");
+    printf("At: %c\n", *c);
+
+    char second_term_buff[length];
+    char *st_buff = &second_term_buff[0];
+
+   for(int i=0; i < max_tries; i++, c++)
     {
         switch (*c)
         {
-            case 'x':
-                line_data.slope_rise = *(c-1) - '0';
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+    //            printf("%d", (*c)-'0');
+                *st_buff = (*c);
+                st_buff++;
+                count++;
                 break;
-            case '+': case'-':
-                line_data.y_intercept = (*(c+2)) - '0';
-            case ' ':
-                continue;
+            case ';':
+                i=max_tries;
+                break;
         }
     }
 
-    printf("Slope is: %d\nY-intercept is: %d\n", line_data.slope_rise, line_data.y_intercept);
+   int y_interceptconv = strtol(second_term_buff, NULL, 10);
+   printf("converted into: %d\n", y_interceptconv);
 
-    return line_data;
+   line_data.y_intercept = y_interceptconv;
+   line_data.slope_rise = slopeconv;
+
+   printf("Slope is: %d\nY-intercept is: %d\n", line_data.slope_rise, line_data.y_intercept);
+
+   return line_data;
 }
